ab8e66e3532ecae118d89e183ffcc1aa9af0b23f
Anowi4 <36716554+Anowi4@users.noreply.github.com>
2018-02-26 16:02:45 +0000

    Add files via upload

---
+++ dog years.py
@@ -0,0 +1,7 @@
+print "\t\t\tspeed calculator"
+distance = int(raw_input("t\tput down the given distance\n"))
+time = int(raw_input("put down the given time\n"))
+spe_ed =distance*time
+print "\ndo you know that you are moving at a speed of",spe_ed,"meter per second"
+raw_input ("press the backspace key to exit programme.")
+print "dog years calculator"
---
+++ speed2.py
@@ -0,0 +1,7 @@
+print "speed calculator"
+distance = int(raw_input("put down the given distance"))
+time = int(raw_input("put down the given time"))
+spe_ed =distance*time
+print "\ndo you know that you are moving at a speed of",spe_ed,"meter per second"
+raw_input ("press the backspace key to exit programme.")
+print "dog years calculator"
